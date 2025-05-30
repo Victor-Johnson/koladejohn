31d2f4de40bd82321518f34877e0c9459d95407c
Victor-Johnson <victorjohnson_979@yahoo.com>
2025-05-30 11:06:02 +0100

    making modifications to the schemas

--- koladebackend/schemas/schemas.py
+++ koladebackend/schemas/schemas.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel ,Emailstr,datetime ,Optional
+from enum import Enum 
 
 app = FastAPI()
 
@@ -24,11 +25,46 @@ class ContactRequest(BaseModel):
     email : Emailstr
     message : str 
 
+class UserRole(BaseModel,Enum):
+    admin = "admin"
+    user = "user"
+
+
+class ProjectTag(str, Enum):
+    python = "Python"
+    pandas = "Pandas"
+    scikit_learn = "scikit-learn"
+    tensorflow = "TensorFlow"
+    nlp = "NLP"
+    forecasting = "Time Series Forecasting"
+    docker = "Docker"
+    mlops = "MLOps"
+    dataeng = "Data Engineering"
+    cloud = "Cloud"
+    aws = "AWS"
+    fastapi = "Fastapi"
+    Com_vision = "Computer Vision"
+    maclearn = "Machine Learning"
+    life = "Life & Fun"
+
+class BlogTag(str, Enum):
+    ml_theory = "ML Theory"
+    data_visualization = "Data Visualization"
+    fastapi = "Exploring FastAPI"
+    ethics = "AI Ethics"
+    open_source = "Open Source in ML"
+    model_deployment = "Model Deployment"
+
+class applicationdomain(BaseModel,Enum):
+    pass
+
 class Blogpost(BaseModel):
     id : int 
     title : str 
     slug : str 
     content : str 
+    tags : Optional[list[str]]
+    domain : Optional[list[str]]
     category : Optional[str]
     cover_images : Optional[str]
     created_at : datetime 
